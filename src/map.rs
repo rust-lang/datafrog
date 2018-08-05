21523a27c365e6979415d24f2e1955afd9c6f869
sscdotopen <ssc.open@googlemail.com>
2018-08-05 09:18:18 +0200

    Reworked according to Frank's comments

--- src/map.rs
+++ src/map.rs
@@ -5,12 +5,12 @@ use super::{Variable, Relation};
 pub fn map_into<T1: Ord, T2: Ord>(
     input: &Variable<T1>,
     output: &Variable<T2>,
-    mut logic: impl FnMut(&T1)->T2) {
+    logic: impl FnMut(&T1)->T2) {
 
     let results: Vec<T2> = input.recent
         .borrow()
-        .into_iter()
-        .map(|tuple| logic(tuple))
+        .iter()
+        .map(logic)
         .collect();
 
     output.insert(Relation::from_vec(results));
